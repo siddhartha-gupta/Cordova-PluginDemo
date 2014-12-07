87485af54291a8c4392f39964c72f7fdb8a0f7a8
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2014-12-07 15:11:59 +0530

    initial file push

--- pluginDemo/platforms/ios/www/cordova_plugins.js
+++ pluginDemo/platforms/ios/www/cordova_plugins.js
@@ -13,28 +13,12 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
-        "id": "org.apache.cordova.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.console": "0.2.11",
-    "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.dialogs": "0.2.10"
+    "org.apache.cordova.console": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
